d1cc741780ef064cc1a3c2710ec227bf70b45737
Brandon <brandon@bremaweb.com>
2014-01-31 13:34:12 -0600

    add recipe for fly2 potion, add gravity1 potion

--- init.lua
+++ init.lua
@@ -74,4 +74,33 @@ local fly_potion3 = {
 
 affects.registerAffect(fly_potion3)
 
+local gravity_potion1 = {
+	affectid = "gravity1",
+	name = "Level 1 Anti Gravity Potion",
+	stages = {
+				{
+					time = 120,
+					physics = { gravity = .5 },
+					custom = { chance=100, func = function(name, player, affectid)
+						minetest.chat_send_player(name,"Gravity releases it's grasp on you")
+					end,runonce=true}
+				},
+				{
+					time = 240,
+					custom = { chance=50, func = function(name, player, affectid)
+						math.randomseed(os.time())
+						local randgravity = (math.random() * 6)
+						player:set_physics_override({gravity=randgravity})
+						minetest.chat_send_player(name,"You feel funny...") 
+					end
+					}
+				}
+			},
+	onremove = function(name, player, affectid)
+		player:set_physics_override({gravity=1})
+		minetest.chat_send_player(name,"You feel gravity's firm grasp take hold once again!",false)
+	end
+}
+affects.registerAffect(gravity_potion1)
+
 dofile(potions.path.."/items.lua")
--- items.lua
+++ items.lua
@@ -5,8 +5,9 @@ minetest.register_craftitem("potions:fly1",{
 	inventory_image = "potions_fly1.png",
 	on_use = function(itemstack,player,pointed_thing)
 		local name = player:get_player_name()
-		affects.affectPlayer(name,"fly1")
-		itemstack:take_item()
+		if ( affects.affectPlayer(name,"fly1") ) then
+			itemstack:take_item()
+		end
 		return itemstack
 	end
 })
@@ -15,10 +16,12 @@ minetest.register_craftitem("potions:fly2",{
 	description = "Level 2 Fly Potion",
 	stack_max = 1,
 	liquids_pointable = false,
+	inventory_image = "potions_fly2.png",
 	on_use = function(itemstack,player,pointed_thing)
 		local name = player:get_player_name()
-		affects.affectPlayer(name,"fly2")
-		itemstack:take_item()
+		if ( affects.affectPlayer(name,"fly2") ) then
+			itemstack:take_item()
+		end
 		return itemstack
 	end	
 })
@@ -27,13 +30,30 @@ minetest.register_craftitem("potions:fly3",{
 	description = "Level 3 Fly Potion",
 	stack_max = 1,
 	liquids_pointable = false,
+	inventory_image = "potions_fly3.png",
 	on_use = function(itemstack,player,pointed_thing)
 		local name = player:get_player_name()
-		affects.affectPlayer(name,"fly3")
-		itemstack:take_item()
+		if ( affects.affectPlayer(name,"fly3") ) then
+			itemstack:take_item()
+		end
 		return itemstack
 	end	
 })
+
+minetest.register_craftitem("potions:gravity1",{
+	description = "Level 1 Anti Gravity Potion",
+	stack_max = 1,
+	liquids_pointable = false,
+	inventory_image = "potions_gravity1.png",
+	on_use = function(itemstack,player,pointed_thing)
+		local name = player:get_player_name()
+		if ( affects.affectPlayer(name,"gravity1") ) then
+			itemstack:take_item()
+		end
+		return itemstack
+	end	
+})
+
 
 minetest.register_craftitem("potions:fly1_raw",{
 	description = "Level 1 Raw Fly Potion",
@@ -54,8 +74,14 @@ minetest.register_craftitem("potions:fly3_raw",{
 	liquids_pointable = false
 })
 
+minetest.register_craftitem("potions:gravity1_raw", {
+	description = "Level 1 Raw Anti Gravity Potion",
+	stack_max = 1,
+	liquids_pointable = false
+})
 
-local ground_flowers = {
+
+local ground_items = {
 	{ "flowers:dandelion_white", "ground_dandelion_white", "Ground White Dandelion" },
 	{ "flowers:dandelion_yellow", "ground_dandelion_yellow", "Ground Yellow Dandelion" },
 	{ "flowers:rose", "ground_rose", "Ground Rose" },
@@ -63,10 +89,11 @@ local ground_flowers = {
 	{ "flowers:tulip", "ground_tulip", "Ground Tulip" },
 	{ "flowers:viola", "ground_viola", "Ground Viola" },
 	{ "flowers:seaweed", "ground_seaweed", "Ground Seaweed" },
-	{ "flowers:waterlily", "ground_waterlily", "Ground Waterlily" }	
+	{ "flowers:waterlily", "ground_waterlily", "Ground Waterlily" },
+	{ "bones:bones","ground_bones","Ground Bones" }	
 }
 
-for _, data in pairs(ground_flowers) do
+for _, data in pairs(ground_items) do
 	technic.register_grinder_recipe({input=data[1], output="potions:"..data[2]})
 	
 	minetest.register_craftitem("potions:"..data[2],{
@@ -80,7 +107,7 @@ end
 minetest.register_craft({
 	type = "shapeless",
 	output = "potions:fly1_raw",
-	recipe = {"bushes:sugar","potions:ground_rose","potions:ground_seaweed","default:mese_crystal_fragment","vessels:glass_bottle","bucket:bucket_water"}	
+	recipe = {"bushes:sugar","potions:ground_rose","potions:ground_seaweed","default:mese_crystal_fragment","vessels:glass_bottle","bucket:bucket_water","technic:mithril_dust"}	
 })
 
 minetest.register_craft({
@@ -89,3 +116,26 @@ minetest.register_craft({
 		output = "potions:fly1"
 })
 
+minetest.register_craft({
+	type = "shapeless",
+	output = "potions:fly2_raw",
+	recipe = {"bushes:sugar","technic:mithril_dust","potions:ground_rose","potions:ground_seaweed","potions:ground_seaweed","default:mese_crystal","vessels:glass_bottle","bucket:bucket_water","technic:mithril_dust"}	
+})
+
+minetest.register_craft({
+		type="cooking",
+		recipe="potions:fly2_raw",
+		output = "potions:fly2"
+})
+
+minetest.register_craft({
+	type="shapeless",
+	output="postion:gravity1_raw",
+	recipe = {"dye:green","farming:wheat","potions:ground_seaweed","potions:ground_bones","default:mese_crystal_fragment","vessels:glass_bottle","bucket:bucket_water"}
+})
+
+minetest.register_craft({
+		type="cooking",
+		recipe="potions:gravity1_raw",
+		output = "potions:gravity1"
+})
